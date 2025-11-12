6949f082732151c26c56af1c78aa83259ae1bad1
shiragami-shun <shun.shiragami0904@gmail.com>
2025-11-12 10:53:21 +0900

    Django

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.recipe_list, name='recipe_list'),
     path('create/', views.recipe_create, name='recipe_create'),
-    path('<int:pk>/', views.recipe_detail, name='recipe_detail')
+    path('<int:pk>/', views.recipe_detail, name='recipe_detail'),
+    path('<int:pk>/edit/', views.recipe_edit, name='recipe_edit'),
+    path('<int:pk>/delete/', views.recipe_delete, name='recipe_delete'),
 ]
