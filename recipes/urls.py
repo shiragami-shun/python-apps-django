3e146776df787fdedc75e6380c914918dd554afc
shiragami-shun <shun.shiragami0904@gmail.com>
2025-11-12 10:42:30 +0900

    Django

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.recipe_list, name='recipe_list'),
+    path('create/', views.recipe_create, name='recipe_create'),
+    path('<int:pk>/', views.recipe_detail, name='recipe_detail')
 ]
