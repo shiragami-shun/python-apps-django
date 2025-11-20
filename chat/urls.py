f207e37f49a5f611f38b2ae1be9fbb018fffee35
shiragami-shun <shun.shiragami0904@gmail.com>
2025-11-20 12:03:27 +0900

    Django

--- chat/urls.py
+++ chat/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index, name="chat"),
     path("start/", views.start_conversation, name="start_conversation"),
     path("conversations/", views.conversation_list, name="conversation_list"),
+    path("conversation/<int:conversation_id>/delete/", views.conversation_delete, name="conversation_delete"),
     path("conversation/<int:conversation_id>/", views.conversation_detail, name="conversation_detail"),
 ]
