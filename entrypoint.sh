68d902e0d3ab09aeeb85fd83c841ac0c6c9f1641
shiragami-shun <shun.shiragami0904@gmail.com>
2026-01-22 11:37:05 +0900

    AWSの環境設定

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,4 @@
-#!/bin/sh
-set -e
-
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
-echo "Applying migrations..."
-python manage.py migrate --noinput
-
-echo "Starting gunicorn..."
-gunicorn python_apps_django.wsgi:application \
-  --bind 0.0.0.0:8000 \
-  --workers 2 \
-  --threads 4
+#!/bin/bash
+export PYTHONPATH=./vendor
+python3 manage.py migrate --noinput
+python3 -m gunicorn --workers 2 python_apps_django.wsgi --bind 0.0.0.0:8000
