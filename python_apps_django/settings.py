19723878885bcc5aac637a120554f970851475e7
shiragami-shun <shun.shiragami0904@gmail.com>
2026-01-29 11:25:03 +0900

    ローカルの環境設定

--- python_apps_django/settings.py
+++ python_apps_django/settings.py
@@ -16,10 +16,9 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
+load_dotenv(BASE_DIR / ".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
