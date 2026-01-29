e99b24694b5c0ea2e0e54a09af2ea591615dde11
shiragami-shun <shun.shiragami0904@gmail.com>
2026-01-29 12:16:22 +0900

    AI_Todoリストアプリ

--- AI_Todo/models.py
+++ AI_Todo/models.py
@@ -1,11 +1,25 @@
+# AI_Todo/models.py
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class Todo(models.Model):
     title = models.CharField(max_length=200)
-    estimated_minutes = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    done = models.BooleanField(default=False)
+    minutes = models.IntegerField()
+    is_completed = models.BooleanField(default=False)
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.title
--- AI_Todo/views.py
+++ AI_Todo/views.py
@@ -3,5 +3,7 @@ from .models import Todo
 
 
 def todo_list(request):
-    todos = Todo.objects.all()
-    return render(request, "AI_Todo/todo_list.html", {"todos": todos})
+    todos = Todo.objects.select_related("category").all()
+    return render(request, "AI_Todo/todo_list.html", {
+        "todos": todos
+    })
